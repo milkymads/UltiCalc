f65a8a98e89f7e9a5b9967b4333b9d969e00a8e0
theQure <rasmus_kure@hotmail.com>
2014-04-27 12:49:08 +0200

    Testing project

--- UltiCalc/UltiCalc/Main.cs
+++ UltiCalc/UltiCalc/Main.cs
@@ -14,6 +14,8 @@ namespace UltiCalc
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
+
+			Console.WriteLine ("WORKING?");
 		}
 	}
 }
